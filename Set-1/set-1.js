8d30d5a8dd4e88f009484c1bc01865dd7e06c7a1
JOY-JB <joybarua0058@gmail.com>
2024-01-18 20:37:26 +0600

    problem solving set-1 done

--- Set-1/set-1.js
+++ Set-1/set-1.js
@@ -43,3 +43,44 @@ const oddOrEven = (input) =>{
 // oddOrEven(3)
 
 
+
+
+// =================================== Problem 4 ==================================
+// You need to take an integer value as input and tell if it is positive or negative or zero.
+
+const checkNumber = (number) =>{
+    if (number < 0) {
+        console.log("negative");
+    } else if (number > 0) {
+        console.log("positive");
+    } else {
+        console.log("zero");
+    }
+}
+
+// checkNumber(-1)
+
+
+
+// =================================== Problem 5 ==================================
+// Suppose a friend of yours tells you that he would buy a Gucci bag if he had Rs 10,000 or more. Otherwise if he has 5000 or more then he will buy Levis Bag. Otherwise he will buy something from New Market. He also told you that if he can buy a Gucci Bag and if he has more than 20 thousand rupees then he will buy another Gucci Belt.
+
+
+const shoppingDecision = (amount) => {
+    if (amount >= 10000) {
+        console.log("Gucci Bag");
+        
+        if (amount > 20000) {
+            console.log("Gucci Belt");
+        }
+    } else if (amount >= 5000) {
+        console.log("Levis Bag");
+    } else {
+        console.log(
+            "something"
+        );
+    }
+}
+
+
+// shoppingDecision(25400)
