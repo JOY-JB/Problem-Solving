e05c2b558999a3048002b0d310678ee108224287
JOY-JB <joybarua0058@gmail.com>
2024-01-19 21:56:23 +0600

    set-2 all problem solved

--- Set-2/set-2.js
+++ Set-2/set-2.js
@@ -1,86 +1,97 @@
 // =================================== Problem 1 ==================================
-// You have to print the following lines in the same format as –
+// Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
 
-// “Recently I heard that you’ve achieved 95% marks in your exam.
-// This is brilliant!
-// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
 
-const text = `Recently I heard that you’ve achieved 95% marks in your exam.
-This is brilliant!
-I wish you’ll shine in your life!	Good luck with all the barriers(/\\) in your life.`;
 
-// console.log(text);
+const people = [
+    { name: "bondhu-1", age: 25, gender: "male" },
+    { name: "bandhobi-1", age: 30, gender: "female" },
+    { name: "bondhu-2", age: 26, gender: "male" },
+    { name: "bandhobi-2", age: 22, gender: "female" },
+    { name: "bandhobi-3", age: 31, gender: "female" },
+  ];
 
-// =================================== Problem 2 ==================================
-// You need to take two integer value inputs and display their sum, subtraction,
-
-const calculation = (input1, input2) =>{
-    console.log(`${input1} + ${input2} = ${input1 + input2}`);    
-    console.log(`${input1} - ${input2} = ${input1 - input2}`);    
-    console.log(`${input1} * ${input2} = ${input1 * input2}`);    
-    console.log(`${input1} / ${input2} = ${(input1 / input2).toFixed(2)}`);    
+const printNames = (array)=>{
+    const females = array.filter(person => person.gender === "female")
+    
+    const names = females.map(person => person.name)
+    return names
 }
 
-// calculation(5, 2);
+
+// console.log(printNames(people));
+
+
+
+// =================================== Problem 2 ==================================
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+const books = [
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+    { title: "1984", author: "George Orwell", year: 1949 },
+  ];
+
+
+  const bookTitle = (array) => {
+    return array.map(book => book.title)
+  }
+
+// console.log(bookTitle(books));
 
 
 
 // =================================== Problem 3 ==================================
-//  You have to take a non-negative (0 or greater) number and say whether the number is even or odd..
+// Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
 
-const oddOrEven = (input) =>{
 
-    if (input < 0 || typeof(input) !== "number" ) {
-        console.log("please enter a positive number!");
-    }
-    else if(input % 2  === 0){
-        console.log(`even`);
-    } else {
-        console.log(`odd`);
-    }
-}
+const sqrNumber = number => number * number
 
-// oddOrEven(3)
+const doubleNumber = number => 2 * number
 
+const addFiveWithNumber = (number) => 5 + number
+
+const  composeFunctions  = (number) => addFiveWithNumber(doubleNumber(sqrNumber(number)))
+
+
+// console.log(composeFunctions(5));
 
 
 
 // =================================== Problem 4 ==================================
-// You need to take an integer value as input and tell if it is positive or negative or zero.
+// Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
 
-const checkNumber = (number) =>{
-    if (number < 0) {
-        console.log("negative");
-    } else if (number > 0) {
-        console.log("positive");
-    } else {
-        console.log("zero");
-    }
-}
 
-// checkNumber(-1)
+const cars = [
+    { make: "Toyota", model: "Camry", year: 2018 },
+    { make: "Honda", model: "Civic", year: 2021 },
+    { make: "Ford", model: "Mustang", year: 2020 },
+    { make: "nissan", model: "classic", year: 2019 },
+ ];
 
+const sortArr = (arr) => {
+  return  arr.sort((car1 ,car2)=> car1.year - car2.year)
+} 
+
+// console.log(sortArr(cars));
 
 
 // =================================== Problem 5 ==================================
-// Suppose a friend of yours tells you that he would buy a Gucci bag if he had Rs 10,000 or more. Otherwise if he has 5000 or more then he will buy Levis Bag. Otherwise he will buy something from New Market. He also told you that if he can buy a Gucci Bag and if he has more than 20 thousand rupees then he will buy another Gucci Belt.
+// Write a function that searches an array of objects for a specific person by name. If found, modify their age property. Print the updated array.
 
+const persons = [
+  { name: "John", age: 25 },
+  { name: "Jane", age: 30 },
+  { name: "Bob", age: 40 }
+];
 
-const shoppingDecision = (amount) => {
-    if (amount >= 10000) {
-        console.log("Gucci Bag");
-        
-        if (amount > 20000) {
-            console.log("Gucci Belt");
-        }
-    } else if (amount >= 5000) {
-        console.log("Levis Bag");
-    } else {
-        console.log(
-            "something"
-        );
-    }
+const searchAndModifyAge = (persons, name, newAge) =>{
+  return persons.map(person => {
+     if (person.name === name) {
+      return {...person, age: newAge}
+   }
+   return person
+  })
 }
 
-
-// shoppingDecision(25400)
+console.log(searchAndModifyAge(persons, "John", 35));
